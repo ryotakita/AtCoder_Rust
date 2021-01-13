0abb11a6bb7f28fef495fb608c1d52cf843c5c7f
ryota-kita <ryota-kita@kke.co.jp>
2021-01-13 13:01:58 +0900

    ABC184 AC:a,b

--- ABC184/src/bin/a.rs
+++ ABC184/src/bin/a.rs
@@ -1,5 +1,11 @@
 use proconio::{input};
 
 fn main() {
-    todo!();
+    input!{
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+    }
+    println!("{}", a*d - b*c);
 }
--- ABC184/src/bin/b.rs
+++ ABC184/src/bin/b.rs
@@ -1,5 +1,11 @@
 use proconio::{input};
 
 fn main() {
-    todo!();
+    input!{
+        N: i32,
+        first: i64,
+        s: String 
+    }
+
+    println!("{}",s.chars().fold(first, |acc, a| if a == 'o' {acc + 1} else { if acc != 0 {acc - 1} else {acc} }));
 }
