3ca77cca62fb7981d8216ed6cc83da21b4b1ffb5
ryota-kita <ryota-kita@kke.co.jp>
2021-01-14 22:15:55 +0900

    update ABC186 c

--- ABC186/src/bin/c.rs
+++ ABC186/src/bin/c.rs
@@ -1,10 +1,23 @@
+///問題文
+///高橋君は 7 が嫌いです。
+///1 以上 N 以下の整数のうち、
+///10 進法で表しても 8 進法で表しても 
+///7 を含まないような数はいくつありますか？
+
+///制約
+///1 ? N ? 10^5
+///N は整数である。
+
 use proconio::{input};
 
 fn contain_7(n: i64) -> bool{
-    if n < 1 { return false; }
-    match n%8{
-        7 => return true,
-        _ => return contain_7(n/8)
+    if n < 1 { 
+        false
+    }else{
+        match n%8{
+            7 => true,
+            _ => contain_7(n/8)
+        }
     }
 }
 fn main() {
